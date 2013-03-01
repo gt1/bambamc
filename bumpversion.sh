3ac5b1f1e70b0aa280531f27d2b7b5e6d4401f02
German Tischler <gt1@sanger.ac.uk>
2013-03-01 15:50:04 +0000

    add version bumping script

---
+++ bumpversion.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+VERSION=`grep AC_INIT < configure.in | awk -F',' '{print $2}'`
+FIRST=`echo $VERSION | awk -F'.' '{print $1}'`
+SECOND=`echo $VERSION | awk -F'.' '{print $2}'`
+THIRD=`echo $VERSION | awk -F'.' '{print $3}'`
+NEXTTHIRD=`expr ${THIRD} + 1`
+
+awk -v first=${FIRST} -v second=${SECOND} -v third=${THIRD} '/^AC_INIT/ {gsub(first"."second"."third,first"."second"."third+1);print} ; !/^AC_INIT/{print}' < configure.in | \
+	awk -v first=${FIRST} -v second=${SECOND} -v third=${THIRD} '/^LIBRARY_VERSION=/ {gsub("="first"."third"."second,"="first":"third+1"."second);print} ; !/^LIBRARY_VERSION=/{print}' \
+	> configure.in.tmp
+# mv configure.in.tmp configure.in
+
+pushd debian
+dch --distribution UNRELEASED -v ${FIRST}.${SECOND}.${NEXTTHIRD}-1
+popd
